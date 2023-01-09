4668e9ccc059f6b86cd44b3fc041483180c359aa
rimmderes <rimmderes@gmail.com>
2023-01-09 16:33:14 +0000

    added a message to show not stopping at Durham q9

--- arrays.js
+++ arrays.js
@@ -92,3 +92,9 @@ console.log(stoppingAtDurham);
  * Q9.
  * If we are stopping at Durham, print "Yay Durham!" to the terminal. If not print "Aww...".
  */
+
+if (stoppingAtDurham) {
+    console.log("Yay Durham");
+} else {
+    console.log("Aww...");
+}
