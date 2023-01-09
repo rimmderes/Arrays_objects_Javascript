e3d869f6c98ff9142c09f44e6dd7f525db77956d
rimmderes <rimmderes@gmail.com>
2023-01-09 16:18:33 +0000

    added a station q7

--- arrays.js
+++ arrays.js
@@ -74,7 +74,8 @@ console.log(stations);
  * We forgot about Darlington! Add it to the route between Newcastle and York.
  */
 
-// console.log(stations);
+stations.splice(3, 0, "Darlington");
+console.log(stations);
 
 
 
